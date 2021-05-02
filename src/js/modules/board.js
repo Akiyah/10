100ad3929ca02c3b9ab832fedb0b5278e73b920a
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2021-05-03 03:08:00 +0900

    create appendRandomTile

--- src/js/modules/board.js
+++ src/js/modules/board.js
@@ -1,23 +1,37 @@
+const Tile = require("./tile");
+
 class Board {
   constructor() {
     this.tiles = [];
   }
 
-  toMap() {
-    let map = [
+  toMatrix() {
+    let matrix = [
       [null, null, null, null],
       [null, null, null, null],
       [null, null, null, null],
       [null, null, null, null]
     ];
     this.tiles.forEach(tile => {
-      map[tile.y][tile.x] = tile.n;
+      matrix[tile.y][tile.x] = tile.n;
     });
-    return map;
+    return matrix;
   }
 
   appendRandomTile() {
-    this.tiles.push("tile");
+    let emptyCells = [];
+    for (let x = 0; x < 4; x++) {
+      for (let y = 0; y < 4; y++) {
+        if (!this.tiles.some(tile => { return tile.x == x && tile.y == y; })) {
+          emptyCells.push([x, y]);
+        }
+      }
+    }
+
+    const cell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    const tile = new Tile(cell[0], cell[1], Math.floor(Math.random() * 2));
+
+    this.tiles.push(tile);
   }
 
   move(direction) {
--- test/js/modules/board.test.js
+++ test/js/modules/board.test.js
@@ -7,11 +7,11 @@ test('constructor', () => {
   expect(board.tiles.length).toBe(0);
 });
 
-describe('#toMap', () => {
+describe('#toMatrix', () => {
   test('emptty', () => {
     const board = new Board();
-    const map = board.toMap()
-    expect(map).toEqual([
+
+    expect(board.toMatrix()).toEqual([
       [null, null, null, null],
       [null, null, null, null],
       [null, null, null, null],
@@ -21,10 +21,9 @@ describe('#toMap', () => {
 
   test('1 tile', () => {
     const board = new Board();
-    const tile = new Tile(1, 2, 5);
-    board.tiles.push(tile);
-    const map = board.toMap()
-    expect(map).toEqual([
+    board.tiles.push(new Tile(1, 2, 5));
+
+    expect(board.toMatrix()).toEqual([
       [null, null, null, null],
       [null, null, null, null],
       [null,    5, null, null],
@@ -34,14 +33,26 @@ describe('#toMap', () => {
 
   test('16 tiles', () => {
     const board = new Board();
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 4; j++) {
-        const tile = new Tile(i, j, i + j * 4);
-        board.tiles.push(tile);
-      }  
-    }
-    const map = board.toMap()
-    expect(map).toEqual([
+    board.tiles = [
+      new Tile(0, 0, 0),
+      new Tile(1, 0, 1),
+      new Tile(2, 0, 2),
+      new Tile(3, 0, 3),
+      new Tile(0, 1, 4),
+      new Tile(1, 1, 5),
+      new Tile(2, 1, 6),
+      new Tile(3, 1, 7),
+      new Tile(0, 2, 8),
+      new Tile(1, 2, 9),
+      new Tile(2, 2, 10),
+      new Tile(3, 2, 11),
+      new Tile(0, 3, 12),
+      new Tile(1, 3, 13),
+      new Tile(2, 3, 14),
+      new Tile(3, 3, 15)
+    ];
+
+    expect(board.toMatrix()).toEqual([
       [ 0,  1,  2,  3],
       [ 4,  5,  6,  7],
       [ 8,  9, 10, 11],
@@ -50,11 +61,50 @@ describe('#toMap', () => {
   });
 });
 
-test('#appendRandomTile', () => {
-  const board = new Board();
-  board.appendRandomTile();
+describe('#appendRandomTile', () => {
+  test('empty', () => {
+    const board = new Board();
+    board.appendRandomTile();
 
-  expect(board.tiles.length).toBe(1);
+    expect(board.tiles.length).toBe(1);
+    expect(board.tiles[0].n == 0 || board.tiles[0].n == 1).toBeTruthy();
+  });
+
+  test('exists 1 tile', () => {
+    const board = new Board();
+    board.tiles.push(new Tile(1, 2, 5));
+    board.appendRandomTile();
+
+    expect(board.tiles.length).toBe(2);
+    expect(board.tiles[1].n == 0 || board.tiles[1].n == 1).toBeTruthy();
+    expect(board.tiles[1].x == 1 && board.tiles[1].y == 2).toBeFalsy();
+  });
+
+  test('exist 15 tiles', () => {
+    const board = new Board();
+    board.tiles = [
+      new Tile(0, 0, 0),
+      new Tile(1, 0, 1),
+      new Tile(2, 0, 2),
+      new Tile(3, 0, 3),
+      new Tile(0, 1, 4),
+      new Tile(1, 1, 5),
+      new Tile(2, 1, 6),
+      new Tile(3, 1, 7),
+      new Tile(0, 2, 8),
+      new Tile(1, 2, 9),
+      new Tile(2, 2, 10),
+      new Tile(3, 2, 11),
+      new Tile(0, 3, 12),
+      new Tile(1, 3, 13),
+      new Tile(2, 3, 14)
+    ];
+    board.appendRandomTile();
+
+    expect(board.tiles.length).toBe(16);
+    expect(board.tiles[15].n == 0 || board.tiles[15].n == 1).toBeTruthy();
+    expect(board.tiles[15].x == 3 && board.tiles[15].y == 3).toBeTruthy();
+  });
 });
 
 test('#move', () => {
