74fb707081e4641c50fb18805fc123c9b924b097
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2021-05-03 21:11:45 +0900

    add rotate methon in tile class

--- src/js/modules/tile.js
+++ src/js/modules/tile.js
@@ -4,6 +4,17 @@ class Tile {
     this.y = y;
     this.n = n;
   }
+
+  toString() {
+    return this.x + "," + this.y + "," + this.n;
+  }
+
+  rotate() {
+    let x = this.y;
+    let y = 3 - this.x;
+    let n = this.n;
+    return new Tile(x, y, n);
+  }
 }
  
 module.exports = Tile;
--- test/js/modules/tile.test.js
+++ test/js/modules/tile.test.js
@@ -7,3 +7,29 @@ test('constructor', () => {
   expect(tile.y).toBe(2);
   expect(tile.n).toBe(5);
 });
+
+test('#toString', () => {
+  expect(new Tile(1, 2, 5).toString()).toBe("1,2,5");
+});
+
+test('#rotate', () => {
+  expect(new Tile(0, 0,  0).rotate().toString()).toBe("0,3,0");
+  expect(new Tile(0, 1,  1).rotate().toString()).toBe("1,3,1");
+  expect(new Tile(0, 2,  2).rotate().toString()).toBe("2,3,2");
+  expect(new Tile(0, 3,  3).rotate().toString()).toBe("3,3,3");
+
+  expect(new Tile(1, 0,  4).rotate().toString()).toBe("0,2,4");
+  expect(new Tile(1, 1,  5).rotate().toString()).toBe("1,2,5");
+  expect(new Tile(1, 2,  6).rotate().toString()).toBe("2,2,6");
+  expect(new Tile(1, 3,  7).rotate().toString()).toBe("3,2,7");
+
+  expect(new Tile(2, 0,  8).rotate().toString()).toBe("0,1,8");
+  expect(new Tile(2, 1,  9).rotate().toString()).toBe("1,1,9");
+  expect(new Tile(2, 2, 10).rotate().toString()).toBe("2,1,10");
+  expect(new Tile(2, 3, 11).rotate().toString()).toBe("3,1,11");
+
+  expect(new Tile(3, 0, 12).rotate().toString()).toBe("0,0,12");
+  expect(new Tile(3, 1, 13).rotate().toString()).toBe("1,0,13");
+  expect(new Tile(3, 2, 14).rotate().toString()).toBe("2,0,14");
+  expect(new Tile(3, 3, 15).rotate().toString()).toBe("3,0,15");
+});
